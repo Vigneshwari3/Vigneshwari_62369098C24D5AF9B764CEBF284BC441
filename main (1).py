1cf261f2796bb04df1113ec435c35d4d3295c205
Vigneshwari3 <145649201+Vigneshwari3@users.noreply.github.com>
2023-10-13 12:30:55 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,5 +1,23 @@
-num = int(input("enter the year"))
-if num % 4 == 0:
-  print("leap year")
-else:
-  print("not leap year")
+class player:
+
+  def play(self):
+    print("The player is playing cricket")
+
+
+class Batsman(player):
+
+  def play(self):
+    print("The batsman is batting")
+
+
+class Bowler(player):
+
+  def play(self):
+    print("The bowler is bowling")
+
+
+batsman = Batsman()
+bowler = Bowler()
+
+batsman.play()
+bowler.play()
